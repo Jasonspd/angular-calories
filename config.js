1f14c1660fb11adcb67f0cc87495e57b2fc616cd
Jason Luu <jasoncluu@googlemail.com>
2016-06-04 21:38:14 +0100

    fixed again

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-	db: process.env.DB || require("./keys.json");.db,
-	key: process.env.KEY || require("./keys.json");.fatSecretKey
+	db: process.env.DB || require("./keys.json").db,
+	key: process.env.KEY || require("./keys.json").fatSecretKey
 }
